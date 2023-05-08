7137299088f63f77ea1ddec03b7295c171d07c75
Sebastien Stordeur <stordeursebastien0@gmail.com>
2023-05-08 11:08:42 +0200

    Code challenge for spotify

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,36 @@
 "use client";
+import axios from "axios";
 import { NextPage } from "next";
 import React, { useEffect } from "react";
+import { useParams } from "next/navigation";
+import { generateRandomString } from "./utils/Spotify/CodeVerifier";
 
 const Home: NextPage = () => {
   const clientId = process.env.NEXT_PUBLIC_CLIENT_ID;
   const clientSecret = process.env.NEXT_PUBLIC_CLIENT_SECRET;
+  const redirectUri = "http://localhost:3000/";
+  const params = useParams();
 
-  const authOptions = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-    body:
-      "grant_type=client_credentials&client_id=" +
-      clientId +
-      "&client_secret=" +
-      clientSecret,
+  generateRandomString(15);
+
+  const authData = {
+    grant_type: "client_credentials",
+    client_id: clientId,
+    client_secret: clientSecret,
   };
 
+  const fetchSpotifyToken = () => {
+    axios
+      .post("https://accounts.spotify.com/api/token", authData, {
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      })
+      .then((response) => console.log(response));
+  };
+
+  const authenticatedUser = () => {};
+
   useEffect(() => {
-    fetch("https://accounts.spotify.com/api/token", authOptions)
-      .then((response) => response.json())
-      .then((data) => console.log(data))
-      .catch((error) => console.error(error));
+    fetchSpotifyToken();
   }, []);
 
   return (
---
+++ src/app/utils/Spotify/CodeVerifier.ts
@@ -0,0 +1,29 @@
+export function generateRandomString(length: number) {
+  let text: string = "";
+  let possible: string =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (let i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+
+  console.log(text);
+  return text;
+}
+
+//code challenge
+
+async function generateCodeChallenge(codeVerifier: string) {
+  function base64encode(string: string) {
+    return window
+      .btoa(String.fromCharCode.apply(null, new Uint8Array(string)))
+      .replace(/\+/g, "-")
+      .replace(/\//g, "_")
+      .replace(/=+$/, "");
+  }
+  const encoder = new TextEncoder();
+  const data = encoder.encode(codeVerifier);
+  const digest = await window.crypto.subtle.digest("SHA-256", data);
+
+  return base64encode(digest);
+}
