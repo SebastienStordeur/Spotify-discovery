857845cb1086ad9366e8c6e929203de548f7aa9b
Sebastien Stordeur <stordeursebastien0@gmail.com>
2023-05-08 10:15:09 +0200

    Get spotify token at each load of page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,35 @@
-import React from 'react'
+"use client";
+import { NextPage } from "next";
+import React, { useEffect } from "react";
 
-const Home = () => {
+const Home: NextPage = () => {
+  const clientId = process.env.NEXT_PUBLIC_CLIENT_ID;
+  const clientSecret = process.env.NEXT_PUBLIC_CLIENT_SECRET;
+
+  const authOptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body:
+      "grant_type=client_credentials&client_id=" +
+      clientId +
+      "&client_secret=" +
+      clientSecret,
+  };
+
+  useEffect(() => {
+    fetch("https://accounts.spotify.com/api/token", authOptions)
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.error(error));
+  }, []);
+
   return (
     <div>
-      <button>TEST</button>
+      <button className="border-2">TEST</button>
     </div>
-    
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
---
+++ src/app/pages/AuthPage.tsx
@@ -0,0 +1,8 @@
+import { NextPage } from "next";
+import React from "react";
+
+const AuthPage: NextPage = () => {
+  return <div>AuthPage</div>;
+};
+
+export default AuthPage;
