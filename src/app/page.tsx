c9f37e0e277b98cc00610f58f269b6c79cc3e419
Sebastien Stordeur <stordeursebastien0@gmail.com>
2023-05-09 18:23:34 +0200

    Authentication flow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,48 +1,25 @@
 "use client";
 import { NextPage } from "next";
-import React, { useEffect } from "react";
-import { generateRandomString } from "@/Spotify/CodeVerifier";
+import React, { useContext, useEffect } from "react";
+import { AuthContext } from "@/store/AuthContext";
 
 const Home: NextPage = () => {
-  const CLIENT_ID = process.env.NEXT_PUBLIC_CLIENT_ID;
-  const redirectUri = "http://localhost:3000/";
-
-  const getParamFromAPI = (hash: string) => {
-    const string = hash.substring(1); //string after hash #
-    const params = string.split("&");
-
-    const splitUpParams = params.reduce((acc: any, currentValue: string) => {
-      const [key, value]: any = currentValue.split("=");
-      acc[key] = value;
-      return acc;
-    }, {});
-
-    return splitUpParams;
-  };
-
-  const state = generateRandomString(16);
-  const scope = "user-read-private user-read-email";
-
-  const url = `https://accounts.spotify.com/authorize?response_type=token&client_id=${CLIENT_ID}&scope=${scope}&redirect_uri=${redirectUri}&state=${state}&show_dialog=true`;
-
-  const handleLogin = () => {
-    window.location = url;
-  };
+  const authCtx = useContext(AuthContext);
 
   useEffect(() => {
-    if (window !== "undefined" && window.location.hash) {
-      const { access_token, expires_in, token_type } = getParamFromAPI(
-        window.location.hash
-      );
-      localStorage.clear();
-      localStorage.setItem("token", access_token);
-      localStorage.setItem("tokenType", token_type);
-      localStorage.setItem("expires_in", expires_in); // will recalculate the time to send another request to get a new token once
-    }
-  }, [getParamFromAPI]);
+    const checkAuthentication = async () => {
+      //check for any
+      const isAuthenticated: any = await authCtx.authDataHandler();
+      if (isAuthenticated) {
+        window.location.href = "http://localhost:3000/profile";
+      }
+    };
+    checkAuthentication();
+  }, []);
+
   return (
     <div>
-      <button className="border-2" onClick={handleLogin}>
+      <button className="border-2" onClick={authCtx.login}>
         TEST
       </button>
     </div>
--- src/store/AuthContext.tsx
+++ src/store/AuthContext.tsx
@@ -1,27 +1,58 @@
 "use client";
+import { generateRandomString } from "@/Spotify/CodeVerifier";
 import { createContext, useState } from "react";
 import React from "react";
-/* import { useRouter } from "next/router"; */
+
+const CLIENT_ID = process.env.NEXT_PUBLIC_CLIENT_ID;
+const REDIRECT_URI = "http://localhost:3000/";
 
 export const AuthContext = createContext({
   isAuthenticated: false,
   login: () => {},
+  authDataHandler: () => {},
   logout: () => {},
 });
+
+const getParamsFromAPI = (hash: string) => {
+  const stringBehindHash = hash.substring(1);
+  const params = stringBehindHash.split("&");
+
+  const splitUpParams = params.reduce((accumulator: Record<string, string>, currentValue: string) => {
+    const [key, value] = currentValue.split("=");
+    accumulator[key] = value;
+    return accumulator;
+  }, {});
+
+  return splitUpParams;
+};
 
 const AuthContextProvider = ({ children }: { children: React.ReactNode }) => {
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
 
-  /* const router = useRouter(); */
+  const state = generateRandomString(16);
+  const scope = "user-read-private user-read-email";
+  const spotifyUrl = `https://accounts.spotify.com/authorize?response_type=token&client_id=${CLIENT_ID}&scope=${scope}&redirect_uri=${REDIRECT_URI}&state=${state}&show_dialog=true`;
+  //const token = localStorage.getItem("token");
 
-  const login = () => {};
+  const login = async () => {
+    window.location.href = spotifyUrl;
+  };
+
+  const authDataHandler: () => boolean = () => {
+    if (window.location.hash) {
+      const { access_token, expires_in, token_type } = getParamsFromAPI(window.location.hash);
+      localStorage.setItem("token", access_token);
+      setIsAuthenticated(true);
+      return true;
+    }
+    return false;
+  };
 
   const logout = () => {
     localStorage.clear();
-    /* router.push("/"); */
   };
 
-  const values = { isAuthenticated, login, logout };
+  const values = { isAuthenticated, login, logout, authDataHandler };
 
   return <AuthContext.Provider value={values}>{children}</AuthContext.Provider>;
 };
