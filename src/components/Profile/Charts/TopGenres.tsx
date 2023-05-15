14cfe7a52356a6ca0c3ab1d8dee9a14cfbec80e9
Sebastien Stordeur <stordeursebastien0@gmail.com>
2023-05-15 09:47:38 +0200

    displaying a correct chart with multiple colors (need to make them less constrast), + custom legend

--- src/components/Profile/Charts/TopGenres.tsx
+++ src/components/Profile/Charts/TopGenres.tsx
@@ -1,15 +1,36 @@
-import { Cell, Pie, PieChart, ResponsiveContainer } from "recharts";
+import { Cell, Pie, PieChart, ResponsiveContainer, Legend } from "recharts";
+
+const CustomLegend = ({ data, colors }: { data: any[]; colors: string[] }) => {
+  return (
+    <ul>
+      {data.map((entry, index) => {
+        const color = colors[index];
+        console.log(color);
+        return (
+          <li key={`flex items-center legend-${index}`} className="relative">
+            <span className="absolute w-2 h-2 top-2" style={{ backgroundColor: colors[index] }} />
+            <span className="ml-4" style={{ color: colors[index] }}>
+              {entry.genre}
+            </span>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
 
 const TopGenres = ({ data }: { data: any[] }) => {
-  console.log(data);
-  const COLORS = ["#FF0000", "#FFF"];
+  const COLORS = ["#FF0000", "#FFF555", "#999DDD", "#0088FE", "#00C49F", "#FFBB28", "#FF8042", "#55555", "#333485"];
+
   return (
-    <ResponsiveContainer width="100%" height="100%">
-      <PieChart width={500} height={300}>
-        <Pie data={data} dataKey="count" nameKey="genre" cx="50%" cy="50%" outerRadius={80} fill="#82ca9d" label />
-        {data.map((entry: any, index: number) => (
-          <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-        ))}
+    <ResponsiveContainer width="50%" height="40%">
+      <PieChart width={200} height={200}>
+        <Pie data={data} dataKey="count" nameKey="genre" cx="50%" cy="50%" outerRadius={80} label>
+          {data.map((_, index: number) => (
+            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+          ))}
+        </Pie>
+        <Legend content={<CustomLegend data={data} colors={COLORS} />} />
       </PieChart>
     </ResponsiveContainer>
   );
--- src/components/Profile/Favorites/FavoritesSongs.tsx
+++ src/components/Profile/Favorites/FavoritesSongs.tsx
@@ -9,7 +9,6 @@ const FavoritesSongs: FC<FavoriteSongsProps> = ({ data }) => {
   return (
     <div className="flex gap-8 bg-darkbg h-64 px-11 py-8 w-full overflow-hidden relative">
       {data.slice(0, 5).map((song: any) => {
-        /* console.log(song); */
         return (
           <div className="w-52 h-full bg-background relative" key={song.id}>
             <img src={song.images[0].url} alt={song.name} className="w-full h-full object-fit absolute" />
