a93bed2c8616dd43528d0685864a3c742f806262
Sebastien Stordeur <stordeursebastien0@gmail.com>
2023-05-16 13:57:34 +0200

    Auth page design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,11 +21,11 @@ const Home: NextPage = () => {
   }, []);
 
   return (
-    <div className="h-screen flex items-center overflow-hidden">
-      <div className="max-w-md w-full mx-auto px-10 py-20 border min-h-3/5 relative overflow-hidden">
-        <h1 className="font-semibold text-3xl">Sign in with your Spotify account</h1>
+    <div className="h-screen flex items-center overflow-hidden bg-background">
+      <div className="max-w-md w-full mx-auto px-10 py-20 bg-darkbg min-h-3/5 relative overflow-hidden">
+        <h1 className="font-semibold text-2xl md:text-3xl text-center text-white">Sign in with your Spotify account</h1>
         <Button
-          className="w-4/5 px-10 h-14 bg-accent text-white text-xl lg:text-3xl overflow-hidden rounded-full absolute top-1/2 transform-translate-x-1/2 -translate-y-1/2"
+          className="w-4/5 px-10 h-12 md:h-14 bg-accent hover:brightness-110 transition-all duration-500 text-white text-xl lg:text-2xl overflow-hidden rounded-full absolute top-1/2 transform-translate-x-1/2 -translate-y-1/2"
           onClick={authCtx.login}
           aria-label="Sign in with Spotify"
         >
