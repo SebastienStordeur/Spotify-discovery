25bda4eefb48f8292046006b0e61262b118c6b3d
Sebastien Stordeur <stordeursebastien0@gmail.com>
2023-05-18 13:25:04 +0200

    Dynamic navigation bar

--- src/components/Layout/SidePanel/SidePanel.tsx
+++ src/components/Layout/SidePanel/SidePanel.tsx
@@ -1,29 +1,34 @@
 import React, { FC } from "react";
 import Link from "next/link";
+import Image from "next/image";
 import music from "../../../assets/icons/music.svg";
+import profile from "../../../assets/icons/profile.svg";
+import activeMusic from "../../../assets/icons/activeMusic.svg";
+import activeProfile from "../../../assets/icons/activeProfile.svg";
+import { usePathname } from "next/navigation";
 
 const SidePanel: FC = () => {
+  const pathname = usePathname();
+  const links = [
+    { url: "/profile", name: "Profile", icon: profile, activeIcon: activeProfile },
+    { url: "/top-artists", name: "Top artists", icon: music, activeIcon: activeMusic },
+    { url: "/playlists", name: "Playlists", icon: music, activeIcon: activeMusic },
+  ];
+
   return (
     <div className="hidden md:flex items-center h-screen w-24 bg-darkbg fixed">
       <ul className="w-full relative text-xs">
-        <li className="flex justify-center items-center text-center h-16 w-full cursor-pointer hover:bg-accent">
-          <Link href="/profile" className="text-white font-semibold font-xs">
-            <img src="../../../assets/icons/music.svg" alt="" />
-            Profile
-          </Link>
-        </li>
-        <li className="flex justify-center items-center text-center h-20 w-full cursor-pointer hover:bg-accent">
-          <Link href="/playlists" className="text-white text-center font-semibold font-xs">
-            <img src="../../../assets/icons/music.svg" alt="" />
-            Playlists
-          </Link>
-        </li>
-        <li className="flex justify-center items-center text-center h-20 w-full cursor-pointer hover:bg-accent">
-          <Link href="/top-artists" className="text-white text-center font-semibold font-xs">
-            <img src="../../../assets/icons/music.svg" alt="" />
-            Top Artists
-          </Link>
-        </li>
+        {links.map((link) => (
+          <li
+            key={link.name}
+            className={`flex justify-center items-center h-16 w-full cursor-pointer ${pathname === link.url ? "text-accent bg-background" : "text-white"}`}
+          >
+            <Link href={link.url} className="flex flex-col items-center font-semibold font-xs">
+              <Image src={pathname === link.url ? link.activeIcon : link.icon} alt="" width={16} height={16} />
+              <span className="mt-2">{link.name}</span>
+            </Link>
+          </li>
+        ))}
       </ul>
     </div>
   );
--- src/components/Playlists/Artist/Artist.tsx
+++ src/components/Playlists/Artist/Artist.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React, { FC } from "react";
 
 interface ArtistProps {
@@ -7,7 +8,14 @@ interface ArtistProps {
 const Artist: FC<ArtistProps> = ({ artist }) => {
   console.log(artist);
   const { name, images, href } = artist;
-  return <article>{name}</article>;
+  return (
+    <Link href={href}>
+      <article className="flex flex-col items-center h-full w-full text-white text-sm font-semibold tracking-wide transition-all duration-200 hover:scale-110 overflow-hidden">
+        <img src={images[0].url} alt={name} className="h-40 w-40 object-cover rounded-full" />
+        <h3 className="mt-2">{name}</h3>
+      </article>
+    </Link>
+  );
 };
 
 export default Artist;
